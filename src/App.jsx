b233ca565243cc704e3f36d64c8a049c39b49910
Nathaniel Alcala <86519190+UnscriptedLogic@users.noreply.github.com>
2026-05-20 17:31:44 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import {
 const App = () => {
     return (
         <Box>
-            {/* <AssetPreloader /> */}
+            <AssetPreloader />
             <DesktopNotice />
             {/* <Navbar /> */}
             <Routes>
